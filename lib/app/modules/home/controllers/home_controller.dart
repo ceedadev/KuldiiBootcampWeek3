b503ebf26b730974df42b1e72e6df81647ad813f
kuldii <sandikha.rahardi@gmail.com>
2021-12-02 00:17:16 +0700

    Alternatif Jawaban

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -4,22 +4,26 @@ import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
 class HomeController extends GetxController {
+  // sudah mantap ini logicnya..
+  // sebetulnya lebih baik, loading itu jangan dipasang seperti ini
+  // kita bisa manfaatin future builder -> connection state = waiting buat render loading
+  // jadi gak perlu di pasang di oninit
   late List<User> userData = [];
-  var isLoading = true.obs;
+  // var isLoading = true.obs;
 
-  @override
-  void onInit() async {
-    await getUsers();
-    super.onInit();
-  }
+  // @override
+  // void onInit() async {
+  //   await getUsers();
+  //   super.onInit();
+  // }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  // @override
+  // void onReady() {
+  //   super.onReady();
+  // }
 
-  @override
-  void onClose() {}
+  // @override
+  // void onClose() {}
 
   Future getUsers() async {
     Uri url = Uri.parse('https://reqres.in/api/users?page=1');
@@ -27,14 +31,14 @@ class HomeController extends GetxController {
 
     if (response.statusCode == 200) {
       print('Found data');
-      isLoading(false);
+      // isLoading(false);
 
       var responseBody = json.decode(response.body);
       List data = responseBody['data'];
       userData = data.map((data) => User.fromJson(data)).toList();
     } else {
       print('No Data');
-      isLoading(false);
+      // isLoading(false);
     }
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -10,43 +10,88 @@ class HomeView extends GetView<HomeController> {
         title: Text('List User'),
         centerTitle: true,
       ),
-      body: Obx(() {
-        if (controller.isLoading.value) {
-          return Center(child: CircularProgressIndicator());
-        } else {
-          return ListView.builder(
-            itemCount: controller.userData.length,
-            itemBuilder: (context, index) {
-              return Card(
-                elevation: 2,
-                margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+      // kita bisa manfaatin future builder -> connection state = waiting buat render loading
+      body: FutureBuilder(
+        future: controller.getUsers(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (controller.userData.length == 0) {
+              return Center(child: Text("TIDAK ADA DATA"));
+            } else {
+              return ListView.builder(
+                itemCount: controller.userData.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 2,
+                    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text('ID : ${controller.userData[index].id}'),
-                          Text('Name : ${controller.userData[index].firstName} ${controller.userData[index].lastName}'),
-                          Text('Email : ${controller.userData[index].email}'),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('ID : ${controller.userData[index].id}'),
+                              Text('Name : ${controller.userData[index].firstName} ${controller.userData[index].lastName}'),
+                              Text('Email : ${controller.userData[index].email}'),
+                            ],
+                          ),
+                          CircleAvatar(
+                            radius: 32,
+                            backgroundColor: Colors.grey,
+                            foregroundImage: NetworkImage('${controller.userData[index].avatar}'),
+                          )
                         ],
                       ),
-                      CircleAvatar(
-                        radius: 32,
-                        backgroundColor: Colors.grey,
-                        foregroundImage: NetworkImage('${controller.userData[index].avatar}'),
-                      )
-                    ],
-                  ),
-                ),
+                    ),
+                  );
+                },
               );
-            },
-          );
-        }
-      }),
+            }
+          }
+        },
+      ),
+      // body: Obx(() {
+      //   if (controller.isLoading.value) {
+      //     return Center(child: CircularProgressIndicator());
+      //   } else {
+      //     return ListView.builder(
+      //       itemCount: controller.userData.length,
+      //       itemBuilder: (context, index) {
+      //         return Card(
+      //           elevation: 2,
+      //           margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      //           child: Padding(
+      //             padding: const EdgeInsets.all(16),
+      //             child: Row(
+      //               crossAxisAlignment: CrossAxisAlignment.center,
+      //               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //               children: [
+      //                 Column(
+      //                   crossAxisAlignment: CrossAxisAlignment.start,
+      //                   children: [
+      //                     Text('ID : ${controller.userData[index].id}'),
+      //                     Text('Name : ${controller.userData[index].firstName} ${controller.userData[index].lastName}'),
+      //                     Text('Email : ${controller.userData[index].email}'),
+      //                   ],
+      //                 ),
+      //                 CircleAvatar(
+      //                   radius: 32,
+      //                   backgroundColor: Colors.grey,
+      //                   foregroundImage: NetworkImage('${controller.userData[index].avatar}'),
+      //                 )
+      //               ],
+      //             ),
+      //           ),
+      //         );
+      //       },
+      //     );
+      //   }
+      // }),
     );
   }
 }
